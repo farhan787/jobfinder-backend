35eae7aba3707e45d0d60d567a9751b5fe3c8da7
Farhan <farhan@Farhans-Macbook.local>
2020-07-22 03:41:22 +0530

    ADD cors enabled and rate limiter for requests

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
 const ErrorHandler = require('./errors/ErrorHandler');
 
 const PORT = process.env.PORT || 5000;
@@ -12,6 +15,14 @@ db.authenticate()
 		await db.sync();
 	})
 	.catch((err) => console.log('Error', err));
+
+const requestLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+});
+
+app.use(cors());
+app.use(requestLimiter);
 
 require('./startup/routes')(app);
 app.use(ErrorHandler);
