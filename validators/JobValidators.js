ab15ab54cbf88cfcd17a61874b586c51017321bc
Farhan <farhan@Farhans-Macbook.local>
2020-07-30 17:08:44 +0530

    ADD job post data validation

--- validators/JobValidators.js
+++ validators/JobValidators.js
@@ -11,6 +11,21 @@ module.exports = {
 			);
 		}
 
+		if (title) {
+			if (title.length < 3) {
+				throw new ApplicationError(
+					'title should be minimun of length 3',
+					ResponseCodes.unprocessableEntity
+				);
+			}
+			if (title.length > 50) {
+				throw new ApplicationError(
+					'title should be maximum of length 50',
+					ResponseCodes.unprocessableEntity
+				);
+			}
+		}
+
 		if (!description) {
 			throw new ApplicationError(
 				'description is missing from request body',
@@ -18,11 +33,41 @@ module.exports = {
 			);
 		}
 
+		if (description) {
+			if (description.length < 3) {
+				throw new ApplicationError(
+					'description should be minimun of length 3',
+					ResponseCodes.unprocessableEntity
+				);
+			}
+			if (description.length > 50) {
+				throw new ApplicationError(
+					'description should be maximum of length 50',
+					ResponseCodes.unprocessableEntity
+				);
+			}
+		}
+
 		if (!location) {
 			throw new ApplicationError(
 				'location is missing from request body',
 				ResponseCodes.unprocessableEntity
 			);
+		}
+
+		if (location) {
+			if (location.length < 3) {
+				throw new ApplicationError(
+					'location should be minimun of length 3',
+					ResponseCodes.unprocessableEntity
+				);
+			}
+			if (location.length > 50) {
+				throw new ApplicationError(
+					'location should be maximum of length 50',
+					ResponseCodes.unprocessableEntity
+				);
+			}
 		}
 	},
 };
