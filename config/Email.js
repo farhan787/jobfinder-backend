2896c636497f66eda8003e403055ad0989929237
farhan <farhan@MacBook-Air.lan>
2020-08-27 06:33:59 +0530

    ADD config for email service

--- config/Email.js
+++ config/Email.js
@@ -1,9 +1,7 @@
 const EMAIL_CONFIG = {
-	host: process.env.emailHost,
-	port: process.env.emailPort,
-	secure: process.env.emailPort === 465 ? true : false,
 	user: process.env.emailUser,
 	pass: process.env.emailPass,
+	service: process.env.emailService
 };
 
 module.exports = EMAIL_CONFIG;
--- services/Email.js
+++ services/Email.js
@@ -2,16 +2,14 @@ const nodemailer = require('nodemailer');
 const emailConfig = require('../config/Email');
 
 const transporter = nodemailer.createTransport({
-	host: emailConfig.host,
-	port: emailConfig.port,
-	secure: emailConfig.secure,
+	service: emailConfig.service,
 	auth: {
 		user: emailConfig.user,
 		pass: emailConfig.pass,
 	},
 });
 
-const from = 'jobfinder.org.com';
+const from = 'jobfinder1.netflify.app';
 
 async function mailCandidate(candidate, job) {
 	let info = await transporter.sendMail({
@@ -19,6 +17,7 @@ async function mailCandidate(candidate, job) {
 		to: candidate.email,
 		subject: 'Job Application Succesful',
 		text: `Hi ${candidate.name},
+		
 		We are pleased to inform you that your job application for role "${job.title}" has been successfully applied. Recruiter will contact you in few days if your profile match the job requirements.
 		`,
 	});
@@ -30,6 +29,7 @@ async function mailRecruiter(recruiter, candidate, job) {
 		to: recruiter.email,
 		subject: 'New candidate application',
 		text: `Hi ${recruiter.name},
+
 		A new candidate "${candidate.name}" has applied to your job with title "${job.title}". kindly checkout your dashboard to take an action.
 		`,
 	});
@@ -37,7 +37,6 @@ async function mailRecruiter(recruiter, candidate, job) {
 
 module.exports = {
 	mailCandidateAndRecruiter: async (candidate, recruiter, job) => {
-		console.log(emailConfig);
 		await mailCandidate(candidate, job);
 		await mailRecruiter(recruiter, candidate, job);
 	},
